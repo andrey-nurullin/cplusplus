8667d51e0d18b06c749ace08e779f8a3efcf7c17
Nurullin Andrey <antikyo@gmail.com>
2015-11-05 21:54:47 +0300

    Микрорефакторинг шаблонного кода для тестов

--- MyApplication/MyApplication/VectorProcess.cpp
+++ MyApplication/MyApplication/VectorProcess.cpp
@@ -5,7 +5,7 @@
 int SummOfElements(const vector<int> & numbers)
 {
 	int summ = 0;
-	for (int number : numbers)
+	for (const auto & number : numbers)
 	{
 		summ += number;
 	}
--- MyApplication/MyApplicationTests/MyApplicationTests.cpp
+++ MyApplication/MyApplicationTests/MyApplicationTests.cpp
@@ -17,9 +17,15 @@ bool VerifySummOfElements(const vector<int> & inputNumbers, const int & expected
 // Создаем решение, набор тестов
 BOOST_AUTO_TEST_SUITE(VectorProcessingTests)
 
-	BOOST_AUTO_TEST_CASE(SummOfVectorElements)
+	BOOST_AUTO_TEST_CASE(TestOnZeroSumm)
 	{
 		BOOST_CHECK(VerifySummOfElements({}, 0));
+		BOOST_CHECK(VerifySummOfElements({ 0 }, 0));
+		BOOST_CHECK(VerifySummOfElements({ 0, 0 }, 0));
+	}
+
+	BOOST_AUTO_TEST_CASE(TestOnCustomSumm)
+	{
 		BOOST_CHECK(VerifySummOfElements({ 1 }, 1));
 		BOOST_CHECK(VerifySummOfElements({ -1, 1 }, 0));
 		BOOST_CHECK(VerifySummOfElements({ -1, 1, 100 }, 100));
