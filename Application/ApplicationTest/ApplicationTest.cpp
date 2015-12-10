b2d2e900c693404c2b87d74838f4fb23b16dde4b
Nurullin Andrey <antikyo@gmail.com>
2015-12-10 20:02:51 +0300

    Шаблонный проект с тестами

---
+++ Application/ApplicationTest/ApplicationTest.cpp
@@ -0,0 +1,13 @@
+// ApplicationTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+BOOST_AUTO_TEST_SUITE(ApplicationTests)
+
+BOOST_AUTO_TEST_CASE(VectorOutputSorted)
+{
+	BOOST_CHECK(2*2 == 4);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
