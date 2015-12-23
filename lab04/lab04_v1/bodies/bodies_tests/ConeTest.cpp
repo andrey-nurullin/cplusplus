171c31ac6213bbeee35a87964b84853b40007090
Nurullin Andrey <antikyo@gmail.com>
2015-12-23 22:29:38 +0300

    Добавил тест на определение коррентости массы

--- lab04/lab04_v1/bodies/bodies_tests/ConeTest.cpp
+++ lab04/lab04_v1/bodies/bodies_tests/ConeTest.cpp
@@ -18,29 +18,34 @@ struct Cone_
 };
 // Сфера
 BOOST_FIXTURE_TEST_SUITE(Cone, Cone_)
-// является объемным телом
-BOOST_AUTO_TEST_CASE(is_a_body)
-{
-	BOOST_CHECK(static_cast<const CBody*>(&cone));
-}
-// имеет плотность
-BOOST_AUTO_TEST_CASE(has_a_density)
-{
-	BOOST_CHECK_EQUAL(static_cast<const CBody &>(cone).GetDensity(), expectedDensity);
-}
-// имеет радиус
-BOOST_AUTO_TEST_CASE(has_a_radius)
-{
-	BOOST_CHECK_EQUAL(cone.GetRadius(), expectedRadius);
-}
-// имеет высоту
-BOOST_AUTO_TEST_CASE(has_a_height)
-{
-	BOOST_CHECK_EQUAL(cone.GetHeight(), expectedHeight);
-}
-// имеет объем
-BOOST_AUTO_TEST_CASE(has_a_volume)
-{
-	BOOST_CHECK_CLOSE_FRACTION(static_cast<const CBody &>(cone).GetVolume(), expectedVolume, 1e-7);
-}
+	// является объемным телом
+	BOOST_AUTO_TEST_CASE(is_a_body)
+	{
+		BOOST_CHECK(static_cast<const CBody*>(&cone));
+	}
+	// имеет плотность
+	BOOST_AUTO_TEST_CASE(has_a_density)
+	{
+		BOOST_CHECK_EQUAL(static_cast<const CBody &>(cone).GetDensity(), expectedDensity);
+	}
+	// имеет радиус
+	BOOST_AUTO_TEST_CASE(has_a_radius)
+	{
+		BOOST_CHECK_EQUAL(cone.GetRadius(), expectedRadius);
+	}
+	// имеет высоту
+	BOOST_AUTO_TEST_CASE(has_a_height)
+	{
+		BOOST_CHECK_EQUAL(cone.GetHeight(), expectedHeight);
+	}
+	// имеет объем
+	BOOST_AUTO_TEST_CASE(has_a_volume)
+	{
+		BOOST_CHECK_CLOSE_FRACTION(static_cast<const CBody &>(cone).GetVolume(), expectedVolume, 1e-7);
+	}
+	// имеет массу
+	BOOST_AUTO_TEST_CASE(has_a_mass)
+	{
+		BOOST_CHECK_CLOSE_FRACTION(static_cast<const CBody &>(cone).GetMass(), expectedVolume * expectedDensity, 1e-7);
+	}
 BOOST_AUTO_TEST_SUITE_END()
